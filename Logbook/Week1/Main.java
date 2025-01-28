084e761979759fc3925a9bc3296ccaeb9a97c8d6
t31hah <khantalhah786@gmail.com>
2025-01-28 15:03:29 +0000

    Test

--- Logbook/Week1/Main.java
+++ Logbook/Week1/Main.java
@@ -1,4 +1,22 @@
 package Logbook.Week1;
 
+import java.util.Scanner; // Import Scanner to take user input
+
 public class Main {
+    public static void main(String[] args) {
+        // Print "Hello World!" (Original Code)
+        System.out.println("Hello World!");
+
+        // Create a Scanner to get user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user for their name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine(); // Store the name in a variable
+
+        // Print a personalized greeting
+        System.out.println("Hello " + name + "!");
+
+        scanner.close(); // Close the scanner (Good practice)
+    }
 }
