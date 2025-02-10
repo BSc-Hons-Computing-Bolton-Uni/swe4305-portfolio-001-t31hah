949933b8372cb86aa53c1d9bd172018b0705c120
t31hah <khantalhah786@gmail.com>
2025-02-10 09:43:03 +0000

    Test

--- Logbook/Week2/Main.java
+++ Logbook/Week2/Main.java
@@ -1,4 +1,33 @@
 package Logbook.Week2;
 
-public class Main {
+import java.util.Scanner;
+//Extend exercise 6 from Week 1 to output a message if the user is 18 or older.
+class AgeCalculator {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the current year
+        int currentYear = 2025;
+
+        // Ask user for their age
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        // Calculate birth year
+        int birthYear = currentYear - age;
+
+        // Display the birth year
+        System.out.println("You were born in " + birthYear);
+
+        // Check if user is 18 or older
+        if (age >= 18) {
+            System.out.println("You are an adult!");
+        } else {
+            System.out.println("You are under 18.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
 }
